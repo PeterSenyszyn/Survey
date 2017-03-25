b814cab45ef8600b9da7b45f77903d0839647e9f
Peter Senyszyn <petergsenyszyn@gmail.com>
2017-03-24 22:19:14 -0500

    v0.1
    First commit

---
+++ src/pages/home/home.ts
@@ -0,0 +1,60 @@
+import { Component, ViewChild } from '@angular/core';
+
+import { NavController, Slides, ToastController } from 'ionic-angular';
+
+@Component({
+  selector: 'page-home',
+  templateUrl: 'home.html'
+})
+export class HomePage {
+  @ViewChild(Slides) slides: Slides;
+
+  stopClockEnd: any;
+
+  constructor(public navCtrl: NavController, public toastCtrl: ToastController) {
+
+  }
+
+  slideChanged()
+  {
+
+
+
+
+    if (this.slides.getActiveIndex() == 2) //Short-term test with no music
+    {
+      this.slides.lockSwipes(true);
+
+      //1 min from now
+      var countDownDate = new Date();
+      countDownDate.setMinutes(countDownDate.getMinutes() + 1);
+
+      var checkTimeLeft = setInterval(function() {
+        // Get todays date and time
+        var now = new Date().getTime();
+
+        // Find the distance between now an the count down date
+        var distance = countDownDate.getTime() - now;
+
+        if (distance < 0)
+        {
+          clearInterval(checkTimeLeft);
+
+          console.log("expired");
+
+          this.slides.slideTo(3);
+        }
+      }, 1000);
+    }
+
+    else if (this.slides.getActiveIndex() == 3)
+    {
+      console.log("moved!");
+    }
+  }
+
+  getTimeRemaining(endTime)
+  {
+
+  }
+}
